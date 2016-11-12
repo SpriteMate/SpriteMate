5738a941dc8b3d4bf64b8233f4fa6a070e7321c8
jmckella <jmckella@live.unc.edu>
2016-11-12 12:05:37 -0500

    Sets express app up to handle data parsing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // Initialize Express app
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 9001;
 
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -13,7 +14,15 @@ var methodOverride = require('method-override');
 var models  = require('./models');
 var sequelizeConnection = models.sequelize;
 
+// Sets up the Express app to handle data parsing
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// static directory
+app.use(express.static('app/public'));
 
 // PREPARE OUR TABLES 
 // ===============================
@@ -38,6 +47,6 @@ sequelizeConnection.query('SET FOREIGN_KEY_CHECKS = 0')
 
 
 // listen on port 3000
-app.listen(9001, function(){
-	console.log("Listening on port 9001")
+app.listen(PORT, function(){
+	console.log("Listening on PORT " + PORT )
 })
